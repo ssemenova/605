1f46c6ca7176396a703f08ae2487761a1c07140b
pcfox-buf <pcfox@buffalo.edu>
2020-10-04 20:23:58 -0400

    Testing commit process

--- phantom_groups/src/lint2.rs
+++ phantom_groups/src/lint2.rs
@@ -1,4 +1,15 @@
 
+//Borrowing from Sofiya
+extern crate rustc_driver;
+#[macro_use]
+extern crate rustc_lint;
+#[macro_use]
+extern crate rustc_session;
+
+use rustc_driver::plugin::Registry;
+use rustc_lint::{EarlyContext, EarlyLintPass, LintArray, LintContext, LintPass};
+use rustc_ast::ast;
+
 declare_lint! {
 
     USES_GLOBAL,
